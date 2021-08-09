eb98e0d494d664741dcbe748360037c4d885b4ad
molinetmoli <58254619+molinetmoli@users.noreply.github.com>
2021-08-09 09:42:06 +0200

    Update _callback.php

--- _callback.php
+++ _callback.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta name="salesforce-community" content="https://uat-bergec.cs129.force.com/AppMovil">
-	<meta name="salesforce-mode" content="inline-callback">
+	<meta name="salesforce-mode" content="modal-callback">
 	<meta name="salesforce-save-access-token" content="true">
 	<meta name="salesforce-allowed-domains" content="embeddedlogin-test.herokuapp.com">
 	<script src="https://uat-bergec.cs129.force.com/AppMovil/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
